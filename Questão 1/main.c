127a51db8d487429cdb7474f0034b9b316a92351
João Victor <98539736+jambis-prg@users.noreply.github.com>
2024-05-16 07:02:06 -0300

    Mutex nao mais alocado
    Removi a alocação do mutex e fix com que fosse totalmente estatico alem  de disponibilizar os arquivos de teste e remover o printf que diz quando acha a palavra
    ou nao, agora apenas mostrará no final o numero de vezes que uma determinada palavra foi encontrada

--- Questão 1/main.c
+++ Questão 1/main.c
@@ -7,16 +7,16 @@
 #define FILES_COUNT 10
 #define MAX_FILENAME 30
 
-const char* c_SearchedWord = "palavra";
+const char* c_SearchedWord = "e";
 unsigned int wordCount = 0;
 
-pthread_mutex_t mutex;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* FindWord(void* filename)
 {
-    FILE* file = fopen((char *) filename, "rt");
+    FILE* file = fopen((char*)filename, "rt");
 
-    if (file != NULL)
+    if(file != NULL)
     {
         const char* ignorable = " \t\n.,;:!?'\"\\/()[]{}<>-";
         char currentChar = 0;
@@ -25,55 +25,53 @@ void* FindWord(void* filename)
         unsigned int searchWordSize = strlen(c_SearchedWord);
         unsigned int ignorableSize = strlen(ignorable);
 
-        while ((currentChar = fgetc(file)) != EOF)
+        while((currentChar = fgetc(file)) != EOF)
         {
             currentChar = tolower(currentChar);
 
-            if (i >= searchWordSize)
+            if(i >= searchWordSize)
             {
-                // Checando se o caracter após a ultima letra da palavra
-                // que esta dando match é um dos caracteres ignoraveis
+                //Checando se o caracter após a ultima letra da palavra
+                //que esta dando match é um dos caracteres ignoraveis
                 int isIgnorable = 0;
-                for (unsigned int j = 0; j < ignorableSize && isIgnorable == 0; j++)
+                for(unsigned int j = 0; j < ignorableSize && isIgnorable == 0; j++)
                 {
-                    if (currentChar == ignorable[j])
+                    if(currentChar == ignorable[j])
                         isIgnorable = 1;
                 }
 
-                /*
-                 * Se o caracter apos a ultima letra encontrada nao for ignoravel
-                 * entao a palavra encontrada nao eh uma palavra por si so
-                 * mas eh na realidade uma palavra que contem outra palavra
-                 * como a palavra "couve" que contem a palavra "ou"
-                 */
-                if (isIgnorable)
+                //Se o caracter apos a ultima letra encontrada nao for ignoravel
+                //entao a palavra encontrada nao eh uma palavra por si so
+                //mas eh na realidade uma palavra que contem outra palavra
+                //como a palavra "couve" que contem a palavra "ou"
+                if(isIgnorable)
                 {
                     pthread_mutex_lock(&mutex);
-                    wordCount++;
-                    pthread_mutex_unlock(&mutex);
 
-                    printf("Encontrada a palavra no arquivo %s\n", (char*)filename);
+                    wordCount++;
+
+                    pthread_mutex_unlock(&mutex);
                 }
                 
                 i = 0;
             }
 
-            if (currentChar == c_SearchedWord[i])
+            if(currentChar == c_SearchedWord[i])
             {
                 i++;
 
-                // Se o i for um entao antes ela era 0 ou seja esse foi o primeiro
-                // caracter a dar match
-                if (i == 1)
+                //Se o i for um entao antes ela era 0 ou seja esse foi o primeiro
+                //caracter a dar match
+                if(i == 1)
                 {
                     int isIgnorable = 0;
                     for(unsigned int j = 0; j < ignorableSize && isIgnorable == 0; j++)
                     {
-                        if (previousChar == ignorable[j])
+                        if(previousChar == ignorable[j])
                             isIgnorable = 1;
                     }
 
-                    if (!isIgnorable)
+                    if(!isIgnorable)
                         i = 0;
                 }
             }
@@ -86,7 +84,7 @@ void* FindWord(void* filename)
         fclose(file);
     }
     else
-        printf("Thread failed to open file %s\n", (char *) filename);
+        printf("Thread failed to open file %s\n", (char*)filename);
 }
 
 int main()
@@ -104,17 +102,14 @@ int main()
         "text_8.txt",
         "text_9.txt"
     };
-
     pthread_t threads[FILES_COUNT];
-    pthread_mutex_init(&mutex, NULL);
 
-    for (unsigned int i = 0; i < FILES_COUNT; i++)
+    for(unsigned int i = 0; i < FILES_COUNT; i++)
         pthread_create(&threads[i], NULL, FindWord, filenames[i]);
 
-    for (unsigned int i = 0; i < FILES_COUNT; i++)
+    for(unsigned int i = 0; i < FILES_COUNT; i++)
         pthread_join(threads[i], NULL);
 
-    pthread_mutex_destroy(&mutex);
     printf("Word Count: %d\n", wordCount);
     return 0;
 }
