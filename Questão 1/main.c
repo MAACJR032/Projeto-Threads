c56072fee209726a37cc660092969c1a6b04f954
maacj <maacj@g5c03.cin.ufpe.br>
2024-05-17 07:26:32 -0300

    vai

---
+++ Questão 1/main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <pthread.h>
+
+#define FILES_COUNT 10
+#define MAX_FILENAME 30
+
+const char* c_SearchedWord = "e";
+unsigned int wordCount = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* FindWord(void* filename)
+{
+    FILE* file = fopen((char*)filename, "rt");
+
+    if(file != NULL)
+    {
+        const char* ignorable = " \t\n.,;:!?'\"\\/()[]{}<>-";
+        char currentChar = 0;
+        char previousChar = 0;
+        unsigned int i = 0;
+        unsigned int searchWordSize = strlen(c_SearchedWord);
+        unsigned int ignorableSize = strlen(ignorable);
+
+        while ((currentChar = fgetc(file)) != EOF)
+        {
+            currentChar = tolower(currentChar);
+
+            if(i >= searchWordSize)
+            {
+                // Checando se o caracter após a ultima letra da palavra
+                // que esta dando match é um dos caracteres ignoraveis
+                int isIgnorable = 0;
+                for (unsigned int j = 0; j < ignorableSize && isIgnorable == 0; j++)
+                {
+                    if (currentChar == ignorable[j])
+                        isIgnorable = 1;
+                }
+
+                // Se o caracter apos a ultima letra encontrada nao for ignoravel
+                // entao a palavra encontrada nao eh uma palavra por si so
+                // mas eh na realidade uma palavra que contem outra palavra
+                // como a palavra "couve" que contem a palavra "ou"
+                if (isIgnorable)
+                {
+                    pthread_mutex_lock(&mutex);
+
+                    wordCount++;
+
+                    pthread_mutex_unlock(&mutex);
+                }
+                
+                i = 0;
+            }
+
+            if (currentChar == c_SearchedWord[i])
+            {
+                i++;
+
+                // Se o i for um entao antes ela era 0 ou seja esse foi o primeiro
+                // caracter a dar match
+                if (i == 1)
+                {
+                    int isIgnorable = 0;
+                    for (unsigned int j = 0; j < ignorableSize && isIgnorable == 0; j++)
+                    {
+                        if(previousChar == ignorable[j])
+                            isIgnorable = 1;
+                    }
+
+                    if (!isIgnorable)
+                        i = 0;
+                }
+            }
+            else
+                i = 0;
+
+            previousChar = currentChar;
+        }
+
+        fclose(file);
+    }
+    else
+        printf("Thread failed to open file %s\n", (char*)filename);
+}
+
+int main()
+{
+    char filenames[FILES_COUNT][MAX_FILENAME] = 
+    {
+        "text_0.txt",
+        "text_1.txt",
+        "text_2.txt",
+        "text_3.txt",
+        "text_4.txt",
+        "text_5.txt",
+        "text_6.txt",
+        "text_7.txt",
+        "text_8.txt",
+        "text_9.txt"
+    };
+    pthread_t threads[FILES_COUNT];
+
+    for(unsigned int i = 0; i < FILES_COUNT; i++)
+        pthread_create(&threads[i], NULL, FindWord, filenames[i]);
+
+    for(unsigned int i = 0; i < FILES_COUNT; i++)
+        pthread_join(threads[i], NULL);
+
+    printf("Word Count: %d\n", wordCount);
+    return 0;
+}
---
+++ Questão 3/main.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <pthread.h>
+
+#define NUM_CLIENTS 3
+#define MAX_FILE 50
+#define MAX_LINE 100
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// garante-se que o saldo inicial do banco é maior ou igual a soma dos saldos iniciais dos clientes
+double banco_saldo = 100000000;
+
+typedef struct _cliente
+{
+    double saldo;
+    int id;
+    char client_file[MAX_FILE];
+} Cliente;
+
+
+/*
+* Funcionamento do txt:
+* 
+* 1 operação por linha
+* Em cada linha haverá: Nome da operação e se necessário o valor
+* Todas as operações presente nos arquivos txt são apenas depósito, saque e consulta
+*
+*/
+
+
+// Função para colocar palavra em minúsculo
+void lower(char *read_word)
+{
+    for (int i = 0; i < strlen(read_word); i++)
+        read_word[i] = tolower(read_word[i]);
+}
+
+void *client_operation(void *cliente)
+{
+    Cliente *client = (Cliente *) cliente;
+
+    FILE *file = fopen((char *) client->client_file, "rt");
+
+    if (file != NULL)
+    {
+        // Vai ler linha por linha
+        char line[MAX_LINE];
+
+        while (fgets(line, sizeof(line), file) != NULL)
+        {
+            char operation[MAX_LINE];
+            double value = 0;
+
+            /* 
+            * Vai ler operação e valor da linha
+            * Se for lido apenas a operação (Único caso que ocorre é o da consulta)
+            * vai mostrar o saldo, já que o valor de retorno vai ser 1
+            */
+            if (sscanf(line, "%s %lf", operation, &value) != 2)
+            {
+                printf("Saldo do Cliente %d: %.2lf\n", client->id, client->saldo);
+                continue;
+            }
+     
+            lower(operation);
+
+            if (strcmp(operation, "deposito") == 0)
+            {
+                // Checa se a operação é válida
+                if (value >= 0)
+                {
+                    client->saldo += value;
+
+                    pthread_mutex_lock(&mutex);
+                    banco_saldo += value;
+                    pthread_mutex_unlock(&mutex);
+                }
+
+            }
+            else if (strcmp(operation, "saque") == 0)
+            {
+                // Checa se a operação é válida
+                if (value >= 0 && client->saldo >= value)
+                {
+                    client->saldo -= value;
+                    
+                    pthread_mutex_lock(&mutex);
+                    banco_saldo -= value;
+                    pthread_mutex_unlock(&mutex);
+                }
+            }
+        }
+
+        fclose(file);
+    }
+    else
+        printf("Thread failed to open file %s\n", client->client_file);
+}
+
+int main()
+{
+    pthread_t threads[NUM_CLIENTS];
+    Cliente clients[NUM_CLIENTS] = { 1000.00, 1, "cliente_0.txt", 
+                                     2000.00, 2, "cliente_1.txt",
+                                     15000.00, 3, "cliente_2.txt"};
+
+    // Threads dos clientes
+    for (int i = 0; i < NUM_CLIENTS; i++)
+        pthread_create(&threads[i], NULL, client_operation, (void *) &clients[i]);
+
+    for (int i = 0; i < NUM_CLIENTS; i++)
+        pthread_join(threads[i], NULL);
+    
+    // Mostrando saldo final dos clientes e do banco
+    for (int i = 0; i < NUM_CLIENTS; i++)
+        printf("Cliente %d: %.2lf\n", clients[i].id, clients[i].saldo);
+
+    printf("banco: %.2lf\n", banco_saldo);
+
+    return 0;
+}
