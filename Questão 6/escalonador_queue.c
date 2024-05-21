9c9abf99f4833a45cccdf5f5419a0666f2f34921
MAACJR032 <123107373+MAACJR032@users.noreply.github.com>
2024-05-21 07:51:28 -0300

    Questão 6 feita
    O escalonador voltou a ser um loop inifinito e no loop para colocar as threads em execução é checado primeiro a variavel condicional antes de colocar alguma thread para executar

--- Questão 6/escalonador_queue.c
+++ Questão 6/escalonador_queue.c
@@ -5,26 +5,33 @@
 
 #define N 4 // Numero maximo de threads executando simultaneamente
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;//garante que apenas uma thread por vez acesse a lista
-pthread_cond_t emptyConditional = PTHREAD_COND_INITIALIZER;//condicional para fazer a thread do escalonador dormir quando n tem thread na lista e acorda quando uma for inserida
-pthread_cond_t fullConditional = PTHREAD_COND_INITIALIZER;//condicional para fazer a thread do escalonador dormir quando n tem thread na lista e acorda quando uma for inserida
+// garante que apenas uma thread por vez acesse a lista
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-//lista_pronto --> array que representa uma fila de execucao pendente das threads
-//estrutra que tem um ponteiro para a funcao e cada thread da lista e representado por essa estrutura
-Queue* lista_pronto;
+// condicional para fazer a thread do escalonador dormir quando n tem thread na lista e acorda quando uma for inserida
+pthread_cond_t emptyConditional = PTHREAD_COND_INITIALIZER;
+
+// condicional para fazer a thread do escalonador dormir quando n tem thread na lista e acorda quando uma for inserida
+pthread_cond_t fullConditional = PTHREAD_COND_INITIALIZER;
+
+pthread_t threads_exec[N];
+
+// lista_pronto --> array que representa uma fila de execucao pendente das threads
+// estrutra que tem um ponteiro para a funcao e cada thread da lista e representado por essa estrutura
+Queue *lista_pronto;
 int count = 0;
 
-void* executar(void* arg) {
-    Node* node = (Node*)arg;
+void* executar(void *arg)
+{
+    Node *node = (Node*) arg;
     node->thread.func(node->thread.arg);
     free(node);
 
-    //Avisa para o escalonador que uma thread terminou
-    //E que há um núcleo disponivel
+    // Avisa para o escalonador que uma thread terminou
+    // E que há um núcleo disponivel
     pthread_mutex_lock(&mutex);
 
     count--;
-
     pthread_cond_signal(&fullConditional);
     
     pthread_mutex_unlock(&mutex);
@@ -32,57 +39,70 @@ void* executar(void* arg) {
     return NULL;
 }
 
-//entra em loop infinito , bloqueia o mutex para garantir acesso exclusivo a lista
-void* escalonador(void* arg) {
-    pthread_mutex_lock(&mutex);
-    //se a lista tiver vazia , espera o condicional fazendo com que a thread do escalonador durma ate uma nova ser inserida
-    while(lista_pronto->size == 0) {
-        pthread_cond_wait(&emptyConditional, &mutex);
-    }
-
-    //Enquanto tiver thread na lista coloca pra executar
-    while(lista_pronto->size > 0)
+// entra em loop infinito , bloqueia o mutex para garantir acesso exclusivo a lista
+void* escalonador(void* arg)
+{
+    while (true)
     {
-        pthread_t tmp;
-        Node* node = dequeue(lista_pronto);
-        pthread_create(&tmp, NULL, executar, node);
-        count++;
-        //Enquanto os N núcleos estiverem cheios escalonador dorme
-        while(count == N)
-            pthread_cond_wait(&fullConditional, &mutex);
-    }
+        pthread_mutex_lock(&mutex);
 
-    pthread_mutex_unlock(&mutex);
+        // se a lista tiver vazia , espera o condicional fazendo com que a thread do escalonador durma ate uma nova ser inserida
+        while(lista_pronto->size == 0)
+        {
+            pthread_cond_wait(&emptyConditional, &mutex);
+        }
+
+        // Enquanto tiver thread na lista coloca pra executar
+        while(lista_pronto->size > 0)
+        {
+            // Enquanto os N núcleos estiverem cheios escalonador dorme
+            while(count == N)
+            {
+                pthread_cond_wait(&fullConditional, &mutex);
+            }
+
+            pthread_t tmp;
+            Node *node = dequeue(lista_pronto);
+            pthread_create(&tmp, NULL, executar, node);
+            count++;
+        }
+
+        pthread_mutex_unlock(&mutex);
+    }
 }
 
-//adiciona uma nova thread a lista
-void agendar(void (*func)(void*), void* arg) {
+// adiciona uma nova thread a lista
+void agendar(void (*func)(void*), void* arg)
+{
     pthread_mutex_lock(&mutex);//bloqueia o mutex e garante acesso exclusivo a lista
 
     enqueue(lista_pronto, func, arg);
 
-    //avisa a cond para acordar a thread do escalonador
+    // avisa a cond para acordar a thread do escalonador
     pthread_cond_signal(&emptyConditional);
     pthread_mutex_unlock(&mutex);
 }
 
-void funcao_qualquer(void* arg) {
+void funcao_qualquer(void* arg)
+{
     printf("Thread executou\n");
 }
 
-int main() {
+int main()
+{
     lista_pronto = create_queue();
-    //cria a thread do escalonador e inicia a func escalonador
+    
+
+    // cria a thread do escalonador e inicia a func escalonador
     pthread_t thread_escalonador;
     pthread_create(&thread_escalonador, NULL, escalonador, NULL);
 
-    for(int i = 0; i < 4*N; i++) {
-        // Adicione threads a lista_pronto usando a funcao agendar
+    // Adicione threads a lista_pronto usando a funcao agendar
+    for(int i = 0; i < 4*N; i++)
         agendar(funcao_qualquer, NULL);
-    }
     
-    //espera a thread do escalonador terminar(pthread_join) sendo que como ela esta em loop infinito n vai terminar
-    //pthread_join e apenas para evitar que termine antes do esperado
+    // espera a thread do escalonador terminar(pthread_join) sendo que como ela esta em loop infinito n vai terminar
+    // pthread_join e apenas para evitar que termine antes do esperado
     pthread_join(thread_escalonador, NULL);
     destroy_queue(lista_pronto);
     return 0;
