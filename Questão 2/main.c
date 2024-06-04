f302bfbdddb8295a84b77d5c3cc499267ecc222e
João Victor <98539736+jambis-prg@users.noreply.github.com>
2024-06-04 14:26:56 -0300

    Adicionando a questão 2 barreiras
    Como pedido na questão 2, a thread de mesclagem dos sub arrays agora é uma thread criada não é mais chamada a partir da main, além disso, foi adicionada a estrutura de barreiras para a sincronização das threads que organizam os sub arrays que devem terminar primeiro antes da thread de mesclagem iniciar o processo

--- Questão 2/main.c
+++ Questão 2/main.c
@@ -1,122 +1,143 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+#define _XOPEN_SOURCE 600
+
 #include <limits.h>
 #include <memory.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_THREADS 5
 #define MIN_ARRAY_SIZE 20
 
 // Os argumentos para o bubble sort são o tamanho do array
 // e o endereço do primeiro elemento do array
-typedef struct
-{
-    int Size, *ArrayBegin;
+typedef struct {
+  int Size, *ArrayBegin;
 } BubbleSortArgs;
 
-void Swap(int *a, int *b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+typedef struct {
+  int *Arr, ArrSize, NumThreads;
+  BubbleSortArgs *Args;
+} MergeSubArraysArgs;
+
+pthread_barrier_t mergeBarrier;
+
+void Swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
 
-void* BubbleSort(void *args)
-{
-    BubbleSortArgs *arrayLimits = (BubbleSortArgs *) args;
+void *BubbleSort(void *args) {
+  BubbleSortArgs *arrayLimits = (BubbleSortArgs *)args;
 
-    for (int i = 0; i < arrayLimits->Size; i++)
-    {
-        for (int j = 0; j < arrayLimits->Size - i - 1; j++)
-        {
-            if (arrayLimits->ArrayBegin[j] > arrayLimits->ArrayBegin[j + 1])
-                Swap(&arrayLimits->ArrayBegin[j], &arrayLimits->ArrayBegin[j + 1]);
-        }
+  for (int i = 0; i < arrayLimits->Size; i++) {
+    for (int j = 0; j < arrayLimits->Size - i - 1; j++) {
+      if (arrayLimits->ArrayBegin[j] > arrayLimits->ArrayBegin[j + 1])
+        Swap(&arrayLimits->ArrayBegin[j], &arrayLimits->ArrayBegin[j + 1]);
     }
+  }
+
+  pthread_barrier_wait(&mergeBarrier);
+
+  return NULL;
 }
 
-/// @brief Mesclar os subarrays organizado (nao eh o algortimo merge sort)
-/// @param arr 
-/// @param arrSize 
-/// @param args 
-/// @param numThreads 
-void MergeSort(int *arr, int arrSize, BubbleSortArgs *args, int numThreads)
-{
-    int newArr[arrSize];
-    int indexes[numThreads];
-    // Zera os indices
-    memset(indexes, 0, sizeof(indexes));
+/// @brief Mesclar os subarrays organizado
+/// @param arrSize
+/// @param args
+/// @param numThreads
+void *MergeSubArrays(void *args) {
+  MergeSubArraysArgs *mergeArrayData = (MergeSubArraysArgs *)args;
 
-    // Se os sub arrays forem por exemplo
-    // 1 5 7 9 - Indice 0
-    // 2 3 4 6 - Indice 0
-    // - - - - - - - - Array Total
-    // Pegamos o menor elemento entre os dois menores elementos desse array
-    // Ou seja comparamos quem é menor 1 ou 2, como o 1 é menor pegamos o 1
-    // e avançamos no subarray escolhido
-    // - 5 7 9 - Indice 1
-    // 2 3 4 6 - Indice 0
-    // 1 - - - - - - - Array total
-    // Agora comparamos 5 com 2 e pegamos o menor e assim por diante
-    for (unsigned int i = 0; i < arrSize; i++)
-    {
-        int min = INT_MAX;
-        int subArrayPicked = 0;
+  int newArr[mergeArrayData->ArrSize];
+  int indexes[mergeArrayData->NumThreads];
+  // Zera os indices
+  memset(indexes, 0, sizeof(indexes));
 
-        // Escolhe o menor valor entre os subarrays
-        for (unsigned int j = 0; j < numThreads; j++)
-        {
-            if(indexes[j] >= args[j].Size)
-                continue;
+  pthread_barrier_wait(&mergeBarrier);
 
-            int n = args[j].ArrayBegin[indexes[j]];
-            if(n < min)
-            {
-                min = n;
-                subArrayPicked = j;
-            }
-        }
+  // Se os sub arrays forem por exemplo
+  // 1 5 7 9 - Indice 0
+  // 2 3 4 6 - Indice 0
+  // - - - - - - - - Array Total
+  // Pegamos o menor elemento entre os dois menores elementos desse array
+  // Ou seja comparamos quem é menor 1 ou 2, como o 1 é menor pegamos o 1
+  // e avançamos no subarray escolhido
+  // - 5 7 9 - Indice 1
+  // 2 3 4 6 - Indice 0
+  // 1 - - - - - - - Array total
+  // Agora comparamos 5 com 2 e pegamos o menor e assim por diante
+  for (unsigned int i = 0; i < mergeArrayData->ArrSize; i++) {
+    int min = INT_MAX;
+    int subArrayPicked = 0;
 
-        indexes[subArrayPicked]++;
-        newArr[i] = min;
+    // Escolhe o menor valor entre os subarrays
+    for (unsigned int j = 0; j < mergeArrayData->NumThreads; j++) {
+      if (indexes[j] >= mergeArrayData->Args[j].Size)
+        continue;
+
+      int n = mergeArrayData->Args[j].ArrayBegin[indexes[j]];
+      if (n < min) {
+        min = n;
+        subArrayPicked = j;
+      }
     }
 
-    // Copia o array organizado para o array original
-    memcpy(arr, newArr, sizeof(newArr));
+    indexes[subArrayPicked]++;
+    newArr[i] = min;
+  }
+
+  // Copia o array organizado para o array original
+  memcpy(mergeArrayData->Arr, newArr, sizeof(newArr));
+
+  return NULL;
 }
 
-int main()
-{
-    int arr[MIN_ARRAY_SIZE] = { 9, 5, 1, 3, 7, 12, 6, 96, 5, 9874, 56, 515, 12, 15, 17, 654, 98, 84, 47, 46 };
+int main() {
+  int arr[MIN_ARRAY_SIZE] = {9,  5,   1,  3,  7,  12,  6,  96, 5,  9874,
+                             56, 515, 12, 15, 17, 654, 98, 84, 47, 46};
 
-    int arrSize = sizeof(arr) / sizeof(int);
+  int arrSize = sizeof(arr) / sizeof(int);
 
-    BubbleSortArgs args[MAX_THREADS];
-    pthread_t threads[MAX_THREADS];
+  BubbleSortArgs args[MAX_THREADS];
+  pthread_t threads[MAX_THREADS];
+  pthread_t mergeThread;
+  MergeSubArraysArgs mergeArg;
 
-    // Inicializando a primeira thread
-    args[0].ArrayBegin = arr;
-    args[0].Size = (arrSize / MAX_THREADS) + (arrSize % MAX_THREADS);
-    pthread_create(&threads[0], NULL, BubbleSort, &args[0]);
-    
-    for (unsigned int i = 1, offset = args[0].Size; i < MAX_THREADS; i++)
-    {
-        args[i].ArrayBegin = arr + offset;
-        args[i].Size = arrSize / MAX_THREADS;
-        offset += args[i].Size;
-        
-        pthread_create(&threads[i], NULL, BubbleSort, &args[i]);
-    }
+  // Cria a barreira que sincroniza todas as threads com os sub arrays +
+  // A thread que mescla os sub arrays para assim formar o array total ordenado
+  pthread_barrier_init(&mergeBarrier, NULL, MAX_THREADS + 1);
 
-    // Esperando todas as threads finalizarem
-    for (unsigned int i = 0; i < MAX_THREADS; i++)
-        pthread_join(threads[i], NULL);
+  // Inicializando a primeira thread
+  args[0].ArrayBegin = arr;
+  args[0].Size = (arrSize / MAX_THREADS) + (arrSize % MAX_THREADS);
+  pthread_create(&threads[0], NULL, BubbleSort, &args[0]);
 
-    MergeSort(arr, arrSize, args, MAX_THREADS);
+  for (unsigned int i = 1, offset = args[0].Size; i < MAX_THREADS; i++) {
+    args[i].ArrayBegin = arr + offset;
+    args[i].Size = arrSize / MAX_THREADS;
+    offset += args[i].Size;
 
-    for (unsigned int i = 0; i < arrSize; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+    pthread_create(&threads[i], NULL, BubbleSort, &args[i]);
+  }
 
-    return 0;
+  mergeArg.Arr = arr;
+  mergeArg.ArrSize = arrSize;
+  mergeArg.NumThreads = MAX_THREADS;
+  mergeArg.Args = args;
+
+  // Criando a thread que mescla os sub arrays
+  pthread_create(&mergeThread, NULL, MergeSubArrays, &mergeArg);
+
+  // Esperando a thread que mescla os sub arrays terminarem
+  pthread_join(mergeThread, NULL);
+
+  pthread_barrier_destroy(&mergeBarrier);
+
+  for (unsigned int i = 0; i < arrSize; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
+
+  return 0;
 }
